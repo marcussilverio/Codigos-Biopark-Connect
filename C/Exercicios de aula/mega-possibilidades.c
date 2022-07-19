3d312086c844223538d8ba4f3db09678467d5df5
Marcus Silverio VM <marcus_vs@live.com>
2022-07-19 14:14:58 -0300

    .

--- C/Exercicios de aula/mega-possibilidades.c
+++ C/Exercicios de aula/mega-possibilidades.c
@@ -11,13 +11,13 @@ int main() {
 
     int contadora = 0;
     //resolucao com if
-    /*
-    for (int d1=1; d1<=60 ; d1++) {
-        for (int d2=(d1+1); d2<=60; d2++) {
-            for (int d3=(d2+1); d3<=60; d3++) {
-                for (int d4=(d3+1); d4<=60; d4++) {
-                    for (int d5=(d4+1); d5<=60; d5++) {
-                        for (int d6=(d5+1); d6<=60; d6++) {
+    
+    for (int d1=2; d1<=60 ; d1++) {
+        for (int d2=1; d2<=60; d2++) {
+            for (int d3=d1+2; d3<=60; d3++) {
+                for (int d4=d2+2; d4<=60; d4++) {
+                    for (int d5=d3+2; d5<=60; d5++) {
+                        for (int d6=d4+2; d6<=60; d6++) {
                             if(d1%2 ==0 && d2%2 ==1 && d3%2 ==0 && d4%2 ==1 && d5%2 ==0 && d6%2 ==1)
                             contadora++;
                         }
@@ -26,8 +26,9 @@ int main() {
             }
         }
     }
-    */
+    
     //resolucao sem if
+    /*
     for (int d1=2; d1<=60 ; d1+=2) {
         for (int d2=(d1+2); d2<=60; d2+=2) {
             for (int d3=(d2+1); d3<=60; d3+=2 ){
@@ -41,6 +42,7 @@ int main() {
             }
         }
     }
+    */
 
 
     printf("\nReposta: %d\n", contadora);
