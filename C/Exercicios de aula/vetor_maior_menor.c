21a49603e6e09b0659c0552da6c183b651e55fb3
Marcus Silverio VM <marcus_vs@live.com>
2022-07-19 14:56:47 -0300

    maior e menor

---
+++ C/Exercicios de aula/vetor_maior_menor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+
+    int i, vet[10];
+    int maior, menor;
+
+    for(i=0; i< 10; i++){
+        scanf("%d", &vet[i]);
+    }
+    maior = menor = vet[0];
+
+    for (i = 1; i< 10; i++){
+        if(vet[i]< menor)
+            menor = vet[i];
+        if(vet[i] > maior)
+            maior = vet[i];
+    }
+
+    printf("maior: %d\nmenor: %d\n", maior, menor);
+    return 0;
+}
