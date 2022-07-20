fa2790f2cda13851f7140be41a7db180a4bc2482
Marcus Silverio VM <marcus_vs@live.com>
2022-07-20 16:17:44 -0300

    define max

--- C/Exercicios de aula/vetor_maior_menor.c
+++ C/Exercicios de aula/vetor_maior_menor.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#define MAX 10
 /*
     Le 10 valores e retorna qual e o menor e maior deles.
 */
 int main(){
 
-    int i, vet[10];
+    int i, vet[MAX];
     int maior, menor;
 
-    for(i=0; i< 10; i++){
+    for(i=0; i< MAX; i++){
         scanf("%d", &vet[i]);
     }
     maior = menor = vet[0];
 
-    for (i = 1; i< 10; i++){
+    for (i = 1; i< MAX; i++){
         if(vet[i]< menor)
             menor = vet[i];
         if(vet[i] > maior)
