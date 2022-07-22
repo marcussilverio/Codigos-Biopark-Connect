1325452a5b6aaf0448db3bebc845eab6ca9066c5
Marcus Silverio <marcus_vs@live.com>
2022-07-22 16:45:26 -0300

    Calculo do dia que sera a pascoa em cada ano com comentarios

--- C/Exercicios de aula/dia_pascoa.c
+++ C/Exercicios de aula/dia_pascoa.c
@@ -3,19 +3,31 @@
 int main(){
 
     int Y, G, C, X, Z, E, N, D;
-    Y = 2023;
-    G = (Y%19)+1;
-    C = (Y/100)+1;
-    X = ((3*C)/4)-12;
-    Z = ((8*C+5)/25)-5;
-    E = (11*G+20+Z-X)%30;
-    if((E==25&&G>11)||E==24)
+
+    // Ano em que se deseja calcular a data da Pascoa
+    Y = 2023; 
+    // Calculo do numero aureo
+    G = (Y%19)+1; 
+    // Calculo do seculo
+    C = (Y/100)+1; 
+    // Calculo de correcoes
+    X = ((3*C)/4)-12; 
+    // Calculo de correcoes
+    Z = ((8*C+5)/25)-5; 
+    // Calculo da Epacta
+    E = (11*G+20+Z-X)%30; 
+    if((E==25&&G>11)||E==24)   
         E++;
-    N = 44-E;
+    // Calculo da lua cheia
+    N = 44-E; 
     if(N<21)
         N+=30;
-    D = ((5*Y)/4)-(X+10);
+    // Calculo do domingo
+    D = ((5*Y)/4)-(X+10); 
+    // Calculo do mes
     N = N+7-((D+N)%7);
+
+    // Resultado
     if(N>31)
         printf("A pascoa sera no dia %d de abril", N-31 );
     else
