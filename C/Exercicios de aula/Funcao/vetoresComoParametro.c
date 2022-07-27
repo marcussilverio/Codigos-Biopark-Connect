a9e709cc0baf232f6fea10b3e3ab41fcedd969d2
Marcus Silverio - W11 G15 <marcus_vs@live.com>
2022-07-27 17:42:53 -0300

    .

--- C/Exercicios de aula/Funcao/vetoresComoParametro.c
+++ C/Exercicios de aula/Funcao/vetoresComoParametro.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void lerVetor(int*, int);
+void zerarVetor(int*, int);
+void imprimeVetor(int*, int);
+int maiorElementoVetor(int*, int);
+int menorELementoVetor(int*, int);
+double mediaElementosVetor(int*, int);
+
+int main(){
+
+    int vet[10];
+
+    lerVetor(vet, 10);
+    imprimeVetor(vet,10);
+    printf("Maior elemento: %d\n", maiorElementoVetor(vet,10));
+    printf("Menor elemento: %d\n", menorELementoVetor(vet,10));
+    printf("Media dos elementos: %.2lf\n", mediaElementosVetor(vet,10));
+    zerarVetor(vet, 10);
+    imprimeVetor(vet,10);   
+    return 0;
+}
 void lerVetor(int *vet, int tam){
     int i;
     printf("Leitura dos numeros do vetor.\n");
@@ -49,18 +69,4 @@ double mediaElementosVetor(int *vet, int tam){
     }
     media = soma/tam;
     return media;
-}
-
-int main(){
-
-    int vet[10];
-
-    lerVetor(vet, 10);
-    imprimeVetor(vet,10);
-    printf("Maior elemento: %d\n", maiorElementoVetor(vet,10));
-    printf("Menor elemento: %d\n", menorELementoVetor(vet,10));
-    printf("Media dos elementos: %lf\n", mediaElementosVetor(vet,10));
-    zerarVetor(vet, 10);
-    imprimeVetor(vet,10);   
-    return 0;
 }
