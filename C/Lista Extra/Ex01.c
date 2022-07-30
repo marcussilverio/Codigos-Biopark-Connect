4c0a78bea99ebe65c874e3de030c7e980ab1579c
Marcus Silverio - WLS2 UBUNTU 20.04 <marcus_vs@live.com>
2022-07-29 22:37:01 -0300

    fnc potencias, passagem de parametro por copia

--- C/Lista Extra/Ex01.c
+++ C/Lista Extra/Ex01.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/*
+    Escreva uma função que computa a potência ab para valores a (double) e b (int) passados por parâmetro
+(não use bibliotecas como math.h). Use a função anterior e crie um programa que imprima todas
+as potências: 2^0, 2^1;..., 2^10; 3^0, ..., 3^10, ..., 10^10
+*/
 double potencia(double a, int b){
     int i, resultadoPotencia=1;
     for(i=0; i< b; i++){
